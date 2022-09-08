173f3f58b92f83838c38e32fdc627f79d094be2d
thiagooncalves <goncalvesthiago1@hotmail.com>
2022-09-08 15:16:50 -0300

    new files reactando

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "some-other-config-you-use",
+        "prettier"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
